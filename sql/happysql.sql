59de398f1685909548778f270713c76a40e5cf6d
Raptarior <qazqww@naver.com>
2021-11-19 14:38:05 +0900

    sql 테이블 데이터 추가

---
+++ sql/happysql.sql
@@ -0,0 +1,242 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema happyhouse
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema happyhouse
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `happyhouse` DEFAULT CHARACTER SET utf8 ;
+USE `happyhouse` ;
+
+-- -----------------------------------------------------
+-- Table `happyhouse`.`sidocode`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `happyhouse`.`sidocode` (
+  `sidoCode` VARCHAR(10) NOT NULL,
+  `sidoName` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`sidoCode`),
+  UNIQUE INDEX `sidoName` (`sidoName` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `happyhouse`.`dongcode`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `happyhouse`.`dongcode` (
+  `dongCode` VARCHAR(10) NOT NULL,
+  `sidoName` VARCHAR(30) NULL DEFAULT NULL,
+  `gugunName` VARCHAR(30) NULL DEFAULT NULL,
+  `dongName` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`dongCode`),
+  INDEX `sidoName` (`sidoName` ASC) VISIBLE,
+  CONSTRAINT `dongcode_ibfk_1`
+    FOREIGN KEY (`sidoName`)
+    REFERENCES `happyhouse`.`sidocode` (`sidoName`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `happyhouse`.`baseaddress`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `happyhouse`.`baseaddress` (
+  `no` INT NOT NULL AUTO_INCREMENT,
+  `sidoName` VARCHAR(30) NULL DEFAULT NULL,
+  `gugunName` VARCHAR(30) NULL DEFAULT NULL,
+  `dongName` VARCHAR(30) NULL DEFAULT NULL,
+  `dongCode` VARCHAR(10) NULL DEFAULT NULL,
+  `lat` VARCHAR(20) NULL DEFAULT NULL,
+  `lng` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`no`),
+  INDEX `dongCode` (`dongCode` ASC) VISIBLE,
+  UNIQUE INDEX `dongCode_UNIQUE` (`dongCode` ASC) VISIBLE,
+  CONSTRAINT `baseaddress_ibfk_1`
+    FOREIGN KEY (`dongCode`)
+    REFERENCES `happyhouse`.`dongcode` (`dongCode`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3597
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `happyhouse`.`guguncode`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `happyhouse`.`guguncode` (
+  `gugunCode` VARCHAR(10) NOT NULL,
+  `gugunName` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`gugunCode`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `happyhouse`.`houseinfo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `happyhouse`.`houseinfo` (
+  `aptCode` INT NOT NULL,
+  `aptName` VARCHAR(50) NULL DEFAULT NULL,
+  `dongCode` VARCHAR(10) NULL DEFAULT NULL,
+  `dongName` VARCHAR(30) NULL DEFAULT NULL,
+  `buildYear` INT NULL DEFAULT NULL,
+  `jibun` VARCHAR(30) NULL DEFAULT NULL,
+  `lat` VARCHAR(30) NULL DEFAULT NULL,
+  `lng` VARCHAR(30) NULL DEFAULT NULL,
+  `img` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`aptCode`),
+  INDEX `dongCode` (`dongCode` ASC) VISIBLE,
+  CONSTRAINT `houseinfo_ibfk_1`
+    FOREIGN KEY (`dongCode`)
+    REFERENCES `happyhouse`.`dongcode` (`dongCode`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `happyhouse`.`housedeal`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `happyhouse`.`housedeal` (
+  `no` INT NOT NULL AUTO_INCREMENT,
+  `aptCode` INT NULL DEFAULT NULL,
+  `dealAmount` VARCHAR(20) NULL DEFAULT NULL,
+  `dealYear` INT NULL DEFAULT NULL,
+  `dealMonth` INT NULL DEFAULT NULL,
+  `dealDay` INT NULL DEFAULT NULL,
+  `area` VARCHAR(30) NULL DEFAULT NULL,
+  `floor` VARCHAR(30) NULL DEFAULT NULL,
+  `type` VARCHAR(30) NULL DEFAULT NULL,
+  `rentMoney` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`no`),
+  INDEX `aptCode` (`aptCode` ASC) VISIBLE,
+  CONSTRAINT `housedeal_ibfk_1`
+    FOREIGN KEY (`aptCode`)
+    REFERENCES `happyhouse`.`houseinfo` (`aptCode`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 68865
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `happyhouse`.`member`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `happyhouse`.`member` (
+  `userid` VARCHAR(16) NOT NULL,
+  `userpwd` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(30) NOT NULL,
+  `email` VARCHAR(30) NULL,
+  `joindate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `happyhouse`.`qna`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `happyhouse`.`qna` (
+  `articleno` INT NOT NULL AUTO_INCREMENT,
+  `userid` VARCHAR(16) NULL DEFAULT NULL,
+  `subject` VARCHAR(100) NULL DEFAULT NULL,
+  `content` VARCHAR(2000) NULL DEFAULT NULL,
+  `hit` INT NULL DEFAULT '0',
+  `regtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`articleno`),
+  INDEX `qna_to_member_fk` (`userid` ASC) VISIBLE,
+  CONSTRAINT `qna_to_member_fk`
+    FOREIGN KEY (`userid`)
+    REFERENCES `happyhouse`.`member` (`userid`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 19
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `happyhouse`.`memo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `happyhouse`.`memo` (
+  `memono` INT NOT NULL AUTO_INCREMENT,
+  `userid` VARCHAR(16) NULL DEFAULT NULL,
+  `comment` VARCHAR(500) NULL DEFAULT NULL,
+  `memotime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `articleno` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`memono`),
+  INDEX `memo_to_qna_fk` (`articleno` ASC) VISIBLE,
+  INDEX `memo_to_member_fk_idx` (`userid` ASC) VISIBLE,
+  CONSTRAINT `memo_to_member_fk`
+    FOREIGN KEY (`userid`)
+    REFERENCES `happyhouse`.`member` (`userid`),
+  CONSTRAINT `memo_to_qna_fk`
+    FOREIGN KEY (`articleno`)
+    REFERENCES `happyhouse`.`qna` (`articleno`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 23
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `happyhouse`.`notice`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `happyhouse`.`notice` (
+  `articleno` INT NOT NULL AUTO_INCREMENT,
+  `userid` VARCHAR(16) NULL DEFAULT NULL,
+  `subject` VARCHAR(100) NULL DEFAULT NULL,
+  `content` VARCHAR(2000) NULL DEFAULT NULL,
+  `hit` INT NULL DEFAULT '0',
+  `regtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`articleno`),
+  INDEX `qna_to_member_fk` (`userid` ASC) VISIBLE,
+  CONSTRAINT `qna_to_member_fk0`
+    FOREIGN KEY (`userid`)
+    REFERENCES `happyhouse`.`member` (`userid`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 19
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `happyhouse`.`favorite`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `happyhouse`.`favorite` (
+  `favno` INT NOT NULL AUTO_INCREMENT,
+  `userid` VARCHAR(16) NULL,
+  `dongCode` VARCHAR(10) NULL,
+  `addtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  INDEX `qna_to_member_fk` (`userid` ASC) VISIBLE,
+  INDEX `fav_to_dongcode_fk_idx` (`dongCode` ASC) VISIBLE,
+  PRIMARY KEY (`favno`),
+  CONSTRAINT `fav_to_member_fk`
+    FOREIGN KEY (`userid`)
+    REFERENCES `happyhouse`.`member` (`userid`),
+  CONSTRAINT `fav_to_dongcode_fk`
+    FOREIGN KEY (`dongCode`)
+    REFERENCES `happyhouse`.`dongcode` (`dongCode`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 19
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
