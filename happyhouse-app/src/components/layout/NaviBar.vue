48187f4957f24cb64e73cc70c2fe94719f32d653
halim <rimha93@gmail.com>
2021-11-25 13:04:51 +0900

    NaviBar 수정

--- happyhouse-app/src/components/layout/NaviBar.vue
+++ happyhouse-app/src/components/layout/NaviBar.vue
@@ -1,9 +1,11 @@
 <template>
   <v-app-bar app flat>
+    <v-app-bar-nav-icon class="hidden-md-and-up" />
+    <v-container class="mx-auto py-0">
       <v-row align="center">
         <router-link to="/">
           <v-img
-          class="mx-5"
+            class="mx-5"
             alt="HappyHouse Logo"
             contain
             src="@/assets/H_logo.png"
@@ -11,87 +13,106 @@
             width="35"
           />
         </router-link>
-        <v-btn large depressed style="font-size: 15px" :to="{ name: 'Notice' }"
+        <v-btn
+          large
+          depressed
+          class="hidden-sm-and-down"
+          style="font-size: 15px"
+          :to="{ name: 'Notice' }"
           >공지사항
         </v-btn>
-        <v-btn large depressed style="font-size: 15px" :to="{ name: 'Board' }">
+        <v-btn
+          large
+          depressed
+          class="hidden-sm-and-down"
+          style="font-size: 15px"
+          :to="{ name: 'Board' }"
+        >
           QnA 게시판
         </v-btn>
-        <v-btn large depressed style="font-size: 15px" :to="{ name: 'House' }">
+        <v-btn
+          large
+          depressed
+          class="hidden-sm-and-down"
+          style="font-size: 15px"
+          :to="{ name: 'House' }"
+        >
           아파트 매매 정보
         </v-btn>
         <v-btn
           large
           depressed
+          class="hidden-sm-and-down"
           style="font-size: 15px"
           :to="{ name: 'Hospital' }"
         >
           코로나 선별 진료소
         </v-btn>
+
+        <v-spacer />
+
+        <div class="ml-auto" v-if="userInfo">
+          <v-menu bottom min-width="250px" rounded offset-y>
+            <template v-slot:activator="{ on }">
+              <v-btn color="white" icon v-on="on">
+                <v-avatar color="secondary">
+                  <v-icon dark> mdi-account-circle </v-icon>
+                </v-avatar>
+              </v-btn>
+            </template>
+            <v-card>
+              <v-list-item-content class="justify-center">
+                <div class="mx-auto text-center">
+                  <v-avatar color="secondary">
+                    <v-icon dark> mdi-account-circle </v-icon>
+                  </v-avatar>
+                  <h3>{{ userInfo.userid }}</h3>
+                  <p class="text-caption mt-1">
+                    {{ userInfo.email }}
+                  </p>
+                  <v-divider class="my-3"></v-divider>
+                  <v-btn depressed rounded text :to="{ name: 'MyFavorite' }">
+                    내 관심지역
+                  </v-btn>
+                  <v-divider class="my-3"></v-divider>
+                  <v-btn depressed rounded text :to="{ name: 'MyPage' }">
+                    내 정보보기
+                  </v-btn>
+                  <v-divider class="my-3"></v-divider>
+                  <v-btn depressed rounded text @click.prevent="onClickLogout">
+                    로그아웃
+                  </v-btn>
+                </div>
+              </v-list-item-content>
+            </v-card>
+          </v-menu>
+        </div>
+        <div v-else>
+          <v-btn
+            color="primary"
+            depressed
+            style="font-size: 15px"
+            :to="{ name: 'SignIn' }"
+          >
+            로그인
+          </v-btn>
+          &nbsp;
+          <v-btn
+            color="secondary"
+            depressed
+            style="font-size: 15px"
+            :to="{ name: 'SignUp' }"
+          >
+            회원가입</v-btn
+          >
+        </div>
       </v-row>
 
-    <div class="ml-auto" v-if="userInfo">
-      <v-menu bottom min-width="250px" rounded offset-y>
-        <template v-slot:activator="{ on }">
-          <v-btn color="white" icon v-on="on">
-            <v-avatar color="indigo">
-              <v-icon dark> mdi-account-circle </v-icon>
-            </v-avatar>
-          </v-btn>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </template>
-        <v-card>
-          <v-list-item-content class="justify-center">
-            <div class="mx-auto text-center">
-              <v-avatar color="indigo">
-                <v-icon dark> mdi-account-circle </v-icon>
-              </v-avatar>
-              <h3>{{ userInfo.userid }}</h3>
-              <p class="text-caption mt-1">
-                {{ userInfo.email }}
-              </p>
-              <v-divider class="my-3"></v-divider>
-              <v-btn depressed rounded text :to="{ name: 'MyFavorite' }">
-                내 관심지역
-              </v-btn>
-              <v-divider class="my-3"></v-divider>
-              <v-btn depressed rounded text :to="{ name: 'MyPage' }">
-                내 정보보기
-              </v-btn>
-              <v-divider class="my-3"></v-divider>
-              <v-btn depressed rounded text @click.prevent="onClickLogout">
-                로그아웃
-              </v-btn>
-            </div>
-          </v-list-item-content>
-        </v-card>
-      </v-menu>
-    </div>
-    <div v-else>
-      <v-btn
-        color="primary"
-        depressed
-        style="font-size: 15px"
-        :to="{ name: 'SignIn' }"
-      >
-        로그인
-      </v-btn>
-      &nbsp;
-      <v-btn
-        color="secondary"
-        depressed
-        style="font-size: 15px"
-        :to="{ name: 'SignUp' }"
-      >
-        회원가입</v-btn
-      >
-    </div>
-
-    <!-- <v-avatar color="indigo">
+      <!-- <v-avatar color="indigo">
       <v-icon dark> mdi-account-circle </v-icon>
     </v-avatar> -->
 
-    <!-- <v-btn
+      <!-- <v-btn
       href="https://github.com/vuetifyjs/vuetify/releases/latest"
       target="_blank"
       text
@@ -99,6 +120,7 @@
       <span class="mr-2">Latest Release</span>
       <v-icon>mdi-open-in-new</v-icon>
     </v-btn> -->
+    </v-container>
   </v-app-bar>
 </template>
 
